e468e909e17a13175d06d5ecb48de0b33c42ef59
kawamorikaito <rf.mori64@gmail.com>
2019-10-31 20:58:52 +0900

    issue https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
